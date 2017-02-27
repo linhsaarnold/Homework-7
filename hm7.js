284384cce24a401e7d809daabc109d3168572ec2
linhsaarnold <linhsa.arnold@gmail.com>
2017-02-26 21:30:19 -0800

    first chunk of hw done

--- hm7.js
+++ hm7.js
@@ -1,19 +1,24 @@
 var locations = [
     [
-        "David Strong Building",
-        48.464801,
-         -123.313539
+        "McPherson Library",
+        48.463449,
+         -123.309662
     ],
     [
-    		"ECS",
-        48.461188,
-        -123.311802
-    ]
+    		"UVIC Bookstore",
+        48.466247,
+        -123.309753
+    ],    
+    [
+        "Student Union Building",
+        48.465173,
+         -123.308397
+    ],
 ]
 
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 15,
-      center: new google.maps.LatLng(48.463649,  -123.311951),
+      center: new google.maps.LatLng(48.463152,  -123.311588),
       mapTypeId: google.maps.MapTypeId.ROADMAP
     });
 
@@ -23,15 +28,28 @@ var locations = [
 
     for (i = 0; i < locations.length; i++) {  
       marker = new google.maps.Marker({
-        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        position: new google.maps.LatLng(locations[i][1], locations[i][2], locations[i][3]),
         map: map
       });
+
+    var count = 0;
 
       google.maps.event.addListener(marker, 'click', (function(marker, i) {
         return function() {
           infowindow.setContent(locations[i][0]);
           infowindow.open(map, marker);
+           if (locations[i][1]) {
+            alert("Latitude,Longitude: 48.463449,-123.309662");
+            alert(count++);
+        } else if (locations[i][2]) {
+            alert("Latitude,Longitude: 48.466247,-123.309753" + count);
+            alert(count++);
+        } else {
+            alert("Latitude,Longitude: 48.465173,-123.308397" + count)
+            alert(count++);
+        };
         }
       })(marker, i));
     }
+
 
